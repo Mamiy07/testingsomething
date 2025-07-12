2acfad5e868a37a10b24a7e9f500191675df187a
Mamiy07 <mamiy7469@gmail.com>
2025-07-12 12:16:37 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+const prisma = new PrismaClient();
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
